3619c3f52e7ac63412bf925b4e8e0a2f9f8b06d9
KSangMin <fhfpszmfnwm@naver.com>
2025-02-19 20:18:33 +0900

    [Feat] 레이싱게임 자동차 기본 좌우 이동 구현

---
+++ Assets/Scripts/Racing/Runner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Runner : MonoBehaviour
+{
+    public List<Transform> tracks;
+    int curId = 2;
+
+    void Start()
+    {
+        curId = 2;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, tracks[curId].position, 0.5f);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        float right = value.Get<float>();
+
+        if (right > 0)
+        {
+            curId = Mathf.Min(++curId, 4);
+        }
+        else if (right < 0)
+        {
+            curId = Mathf.Max(--curId, 0);
+        }
+    }
+}
